7f1cb9f78d1cbb9d653892055a3cc4a7f719f5cd
osteensouthpaw <osteensouthpaw@gmail.com>
2024-10-25 08:50:28 +0300

    add additional loading skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,21 @@
+import { Box, Card, Flex } from "@radix-ui/themes";
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const IssueDetailLoadingPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box className="max-w-xl">
+      <Skeleton width="10rem" />
+      <Flex gap="3" my="2">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card mt="4" className="prose">
+        <Skeleton count={3} />
+      </Card>
+    </Box>
+  );
 };
 
 export default IssueDetailLoadingPage;
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const NewIssueLoadingPage = () => {
   return (
-    <div>Loading...</div>
-  )
-}
+    <div className="space-y-3 max-w-xl">
+      <Skeleton width={10} />
+      <Skeleton height="20rem" />
+      <Skeleton width={5} />
+    </div>
+  );
+};
 
 export default NewIssueLoadingPage;
