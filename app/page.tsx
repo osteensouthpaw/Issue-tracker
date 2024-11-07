bc1d3619c2364f6026620ec65db1d484db538814
osteensouthpaw <osteensouthpaw@gmail.com>
2024-11-07 10:53:00 +0300

    implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <div>Hello World</div>;
+import { Text } from "@radix-ui/themes";
+import Pagination from "./components/Pagination";
+
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  return (
+    <div>
+      <Text>Hello World</Text>
+      <Pagination
+        currentPage={10}
+        itemCount={100}
+        pageSize={parseInt(searchParams.page)}
+      />
+    </div>
+  );
 }
